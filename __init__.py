be578877929cb8a53fd8c47ff146dc5b507c00eb
DESKTOP-HYTE\johnt <trashkollector@gmail.com>
2025-08-28 00:10:33 -0400

    fix issue with mappings

--- __init__.py
+++ __init__.py
@@ -10,8 +10,11 @@ NODE_CLASS_MAPPINGS = {
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
      "TKPromptEnhanced": "Enhanced Prompt w Cam",
-     "TKVideoUserinputs": "Video User Inputs",
+     "TKVideoUserInputs": "Video User Inputs",
      "TKSamplerUserInputs": "Sampler Inputs",
-
-
 }
+
+WEB_DIRECTORY = "./js"
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
+
